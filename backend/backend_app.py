35030cd7a8ad6ff56c0206e400d7cc49edf6afc8
Frelin <fogariojohnson@gmail.com>
2023-07-03 20:52:53 -0500

    Docstring added.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -13,6 +13,13 @@ POSTS = [
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    """
+    Gets the post and allows user to sort using Postman.
+
+    Return:
+        POSTS(json file): The post with title and content.
+
+    """
     sort_field = request.args.get('sort')
     sort_direction = request.args.get('direction')
 
@@ -26,6 +33,12 @@ def get_posts():
 
 @app.route('/api/posts', methods=['POST'])
 def add_post():
+    """
+    Allows user to add a post.
+
+    Return:
+        new_post(json file): The added post with its title, content and id.
+    """
     new_post = request.get_json()
 
     if not new_post or 'title' not in new_post or 'content' not in new_post:
@@ -40,6 +53,15 @@ def add_post():
 
 @app.route('/api/posts/<int:id>', methods=['DELETE'])
 def delete_post(id):
+    """
+    Allows user to delete a post.
+
+    Arg:
+        id(int): The unique id number of the post.
+
+    Return:
+        message(json file): The message on what id of the post has been deleted.
+    """
     post = find_post_by_id(id)
 
     if post is None:
@@ -51,6 +73,15 @@ def delete_post(id):
 
 @app.route('/api/posts/<int:id>', methods=['PUT'])
 def update_post(id):
+    """
+    Allows user to update a post(Postman app).
+
+    Arg:
+        id(int): The unique id number of the post.
+
+    Return:
+       post(json file): The updated post with its content, id, and title.
+    """
     post = find_post_by_id(id)
 
     if post is None:
@@ -66,6 +97,12 @@ def update_post(id):
 
 @app.route('/api/posts/search', methods=['GET'])
 def search_posts():
+    """
+    Allows user to search for a post in a Postman app.
+
+    Return:
+        filtered_posts(json file): The posts based on the search key word.
+    """
     title = request.args.get('title')
     content = request.args.get('content')
 
@@ -83,6 +120,15 @@ def search_posts():
 
 
 def find_post_by_id(post_id):
+    """
+    It looks for the unique id number.
+
+    Arg:
+        id(int): The unique id number of the post.
+
+    Return:
+        post(dict): The post based from the given id number.
+    """
     for post in POSTS:
         if post["id"] == post_id:
             return post
@@ -91,16 +137,24 @@ def find_post_by_id(post_id):
 
 @app.errorhandler(404)
 def not_found_error(error):
+    """It handles the error if the router is not found."""
     return jsonify({"error": "Not Found"}), 404
 
 
 @app.errorhandler(405)
 def method_not_allowed_error(error):
+    """It handles the error if the method is not allowed."""
     return jsonify({"error": "Method Not Allowed"}), 405
 
 
 @app.route('/api/posts', methods=['GET'])
 def page_limit():
+    """
+    It limits the number of pages into 10.
+
+    Return:
+        paginated_posts(json file): the number of posts allowed.
+    """
     page = int(request.args.get('page', 1))
     limit = int(request.args.get('limit', 10))
 
